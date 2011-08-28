fec019efc6fd321543f0f70c45c373fc785a4f9a
Iorgama Porcely <iorgama.p@gmail.com>
2011-08-27 21:17:46 -0300

    atualizando a views de inscrição

--- subscription/views.py
+++ subscription/views.py
@@ -7,29 +7,20 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import send_mail
 from subscription.models import Subscription
  
-
-#def inscricao(request):
-#    if request.method == 'POST':
-#        form = SubscriptionForm(request.POST)
-#        if form.is_valid():
-#            form.save()
-#            messages = (u'Inscrição realizada com sucesso;', u'Aguarde até que sua solicitação de cadastro seja aprovada')
-#            return render_to_response('index.html',{'messages':messages }, context_instance=RequestContext(request))
-#    else:
-#        form = SubscriptionForm()
-#    return render_to_response('subscription/subscription_form.html',{'form': form, 'title' : u'Preencha Sua Inscrição'}, context_instance=RequestContext(request))
-
 def inscricao(request):
     if request.method == 'POST':
         form = SubscriptionForm(request.POST)
         if form.is_valid():
             subscription = form.save()
-            send_mail(
-                subject = u'Inscrição no EventeX',
-                message = u'Obrigado por se inscrever no EventeX!',
-                from_email = 'contato@eventex.com',
-                recipient_list = ['iorgama.p@gmail.com'],
-            )
+            try:
+                send_mail(
+                    subject = u'Inscrição no EventeX',
+                    message = u'Obrigado por se inscrever no EventeX!',
+                    from_email = 'contato@eventex.com',
+                    recipient_list = [request.POST['email']],
+                )
+            except:
+                pass
             return sucesso(request, subscription.pk)
 #            messages = (u'Inscrição realizada com sucesso;', u'Aguarde até que sua solicitação de cadastro seja aprovada')
 #            return render_to_response('index.html',{'messages':messages, 'subscription': subscription.pk}, context_instance=RequestContext(request))
